dafe26cb779171359a2cc6d7860ed9affe878760
Ryszard Pytka <s198323@student.pg.edu.pl>
2026-05-16 00:09:21 +0200

    feat random samples

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import time
 from Tree import Node
 
 BLUE_WINS_INDEX = 0
-
+SAMPLES = 9782
 def gini_left(left_pos, left_neg):
     if left_pos == 0.0 or left_neg == 0.0:
         return 0
@@ -20,18 +20,18 @@ def gini_gain(g_left, g_right, left, right):
         return 0
     return 1 - ((left/(left+right))*g_left) - ((right/(left+right))*g_right) 
 
-def get_gain(series : pd.DataFrame, mean : float, feat : int):
+def get_gain(series : pd.DataFrame, mean : float):
     left_pos, left_neg = 0, 0
     right_pos, right_neg = 0, 0
 
     for r in series.itertuples(index=False):
-        if r[feat] < mean: #left
-            if r[BLUE_WINS_INDEX] == 1:
+        if r[0] < mean: #left
+            if r[1] == 1:
                 left_pos += 1
             else:
                 left_neg += 1
         else:
-            if r[BLUE_WINS_INDEX] == 1:
+            if r[1] == 1:
                 right_pos += 1
             else:
                 right_neg += 1
@@ -50,29 +50,26 @@ def get_gain(series : pd.DataFrame, mean : float, feat : int):
     )
     return result_gain
 
-def find_best_feat(series : pd.DataFrame, feat : int):
+def find_best_feat(series : pd.DataFrame): #(feat, blueWins)
     b_gain = 0.0
-    prev = None
     b_mean = 0.0
+    prev = None
     for r in series.itertuples(index=False):
         if prev is None:
-            prev = r[feat]
+            prev = r[0]
             continue
-        mean = (r[feat] + prev)/2
-        prev = r[feat]
-        g = get_gain(series=series, mean=mean, feat=feat)
+        mean = (r[0] + prev)/2
+        prev = r[0]
+        g = get_gain(series=series, mean=mean)
         if b_gain < g:
             b_gain = g
             b_mean = mean
-            #print(f"New {b_gain} for {b_mean}")
-    #print(f"Best feat {feat}: {b_mean}")
     return b_gain, b_mean
 
 def partition(data : pd.DataFrame, feat : int, mean : float):
     left = []
     right = []
     for r in data.itertuples(index=False):
-        #print(r)
         if float(r[feat]) <= mean:
             left.append(r)
         else:
@@ -81,7 +78,7 @@ def partition(data : pd.DataFrame, feat : int, mean : float):
 
 def check_label(node_data: pd.DataFrame, isLeaf : bool):
     c_1, c_0 = 0, 0
-    for x in node_data['blueWins']:
+    for x in node_data:
         if x == 1:
             c_1 += 1
         elif x == 0:
@@ -94,28 +91,24 @@ def check_label(node_data: pd.DataFrame, isLeaf : bool):
         return 1 if c_1 >= c_0 else 0
     else:
         return 2
-
+    
 def train(data : pd.DataFrame, depth : int, depth_threshold : int):  
-    #print(f"Node in {depth} level!")
-    if depth > depth_threshold: #STOP
-        res = check_label(data, True)
-        #print(f"Depth reached -> DEPTH: {depth} <> LABEL: {res} <> SAMPLES: {len(data)}")
-        return Node(feature_idx=0, threshold=0, left=None, right=None, label = check_label(data, True), is_Leaf=True)
-    res = check_label(data, False)
+    if depth >= depth_threshold: #STOP
+        res = check_label(data['blueWins'], True)
+        return Node(feature_idx=0, threshold=0, left=None, right=None, label = res, is_Leaf=True)
+    res = check_label(data['blueWins'], False)
     if res != 2:
-        #print(f"Labeled leaf -> DEPTH: {depth} <> LABEL: {res} <> SAMPLES: {len(data)}")
         return Node(feature_idx=0, threshold=0, left=None, right=None, label = res, is_Leaf=True)
     else:
         b_feat = None
         b_g, b_mean = 0, 0
-        #print(data.columns)
         for index, feat in enumerate(data.columns):
-            if feat in ['gameId', 'blueWins']:
+            if feat in ['gameId', 'blueWins', 'index']:
                 continue
-            g, mean = find_best_feat(data.sort_values(feat), feat=index)
+            series = data[[feat, 'blueWins']].sort_values(feat)
+            g, mean = find_best_feat(series)
             if b_g < g:
                 b_g = g
-                #print(f"Feat {feat} index {index}")
                 b_feat = index
                 b_mean = mean
         left, right = partition(data, b_feat, b_mean)
@@ -123,11 +116,9 @@ def train(data : pd.DataFrame, depth : int, depth_threshold : int):
 
 #Przejdź po drzewie i w liściu sprawdź odp
 def classify(node : Node, sample) -> int:
-    #print(F"Class sample: {sample}")
     if node.is_Leaf: #Return label of reached leaf
-        return int(node.label)
+        return node.label
     else:
-        #print(f"FEAT_ID: {node.feature_idx} <> THRESHOLD: {node.threshold} <> VALUE: {sample[node.feature_idx]}")
         if sample[node.feature_idx] < node.threshold:
             return classify(node.left, sample)
         else:
@@ -137,7 +128,6 @@ def test(tree_root : Node, data : pd.DataFrame):
     accurate = 0
     for s in data.itertuples(index=False):
         s_class = classify(tree_root, s)
-        #print(s, s[BLUE_WINS_INDEX])
         if s_class == s[BLUE_WINS_INDEX]:
             accurate += 1
     print(f"Accurate -> {accurate} from {len(data)}")
@@ -153,21 +143,23 @@ def print_node(node : Node, depth : int):
         print_node(node.left, depth+1)
         print_node(node.right, depth+1)
 
-def read_data(s : int):
-    data = pd.read_csv('high_diamond_ranked_10min.csv', nrows=s,
-                        usecols=['blueWins','blueWardsPlaced','blueWardsDestroyed','blueFirstBlood','blueKills','blueDeaths','blueAssists','blueEliteMonsters','blueDragons','blueHeralds','blueTowersDestroyed','blueTotalGold','blueTotalExperience','blueTotalMinionsKilled','blueTotalJungleMinionsKilled','blueGoldDiff','blueExperienceDiff','redWardsPlaced','redWardsDestroyed','redAssists','redEliteMonsters','redDragons','redHeralds','redTowersDestroyed','redTotalGold','redTotalExperience','redTotalMinionsKilled','redTotalJungleMinionsKilled']),
-    t1 = int(s*0.2)
-    return data[0][0:t1], data[0][t1:s]
+def read_data(data: pd.DataFrame, s : int):
+    t1 = int(s*0.1)
+    return data[0:t1], data[t1:s]
 
-size = [2000, 4000, 6000, 9000]
+
+data = pd.read_csv('high_diamond_ranked_10min.csv', usecols=['blueWins','blueWardsPlaced','blueWardsDestroyed','blueFirstBlood','blueKills','blueDeaths','blueAssists','blueEliteMonsters','blueDragons','blueHeralds','blueTowersDestroyed','blueTotalGold','blueTotalExperience','blueTotalMinionsKilled','blueTotalJungleMinionsKilled','blueGoldDiff','blueExperienceDiff','redWardsPlaced','redWardsDestroyed','redAssists','redEliteMonsters','redDragons','redHeralds','redTowersDestroyed','redTotalGold','redTotalExperience','redTotalMinionsKilled','redTotalJungleMinionsKilled']),
+rand_data= data[0].sample(frac=1)
+
+size = [2000]
 dept = [1,2,3,4,5]
 for s in size:
-    test_data, training_data  = read_data(s)
-    print(len(test_data), len(training_data))
+    test_data, training_data  = read_data(rand_data, s)
+    #print(test_data, training_data)
     for d in dept:
         start = time.perf_counter()
         tree_root = train(training_data, 1, d)
         stop = time.perf_counter()
-        print(f"{s} size sample training time -> {stop-start} for depth -> {d}")
+        print(f"Training time -> {stop-start} for depth -> {d}")
         result = test(tree_root, test_data)
 
